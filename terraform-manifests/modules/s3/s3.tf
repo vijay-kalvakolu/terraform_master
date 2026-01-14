7158cb516149fdc4046498475e17dcc56f678653
vijay2693 <85749478+vijay2693@users.noreply.github.com>
2026-01-14 18:48:54 +0100

    updated s3 module

--- terraform-manifests/modules/s3/s3.tf
+++ terraform-manifests/modules/s3/s3.tf
@@ -9,12 +9,14 @@ resource "aws_s3_bucket" "tfstate_bucket" {
       }
     }
 
+
     resource "aws_s3_bucket_versioning" "tf_state_versioning" {
         bucket = aws_s3_bucket.tfstate_bucket.id
         versioning_configuration {
           status = "Enabled"
         }
   }
+  
 
   resource "aws_s3_bucket_server_side_encryption_config" "tfstate_encryption" {
         bucket = aws_s3_bucket.tfstate_bucket.id
@@ -25,4 +27,12 @@ resource "aws_s3_bucket" "tfstate_bucket" {
                 }
             }
 
+  }
+
+  resource "aws_s3_bucket_public_access_block" "tfstate_block_public_access" {
+    bucket = aws_s3_bucket.tfstate_bucket.id
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
   }
