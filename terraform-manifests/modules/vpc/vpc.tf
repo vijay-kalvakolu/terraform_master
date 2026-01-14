98ee9d438a23a94be9bb06a5368f111d1f6d9330
vijay2693 <85749478+vijay2693@users.noreply.github.com>
2026-01-14 18:33:28 +0100

    created vpc modeule

--- terraform-manifests/modules/vpc/vpc.tf
+++ terraform-manifests/modules/vpc/vpc.tf
@@ -1,6 +1,16 @@
+# Resource - VPC
 resource "aws_vpc" "main" {
     cidr_block = var.vpc_cidr
     enable_dns_hostnames = true
     enable_dns_support = true
     tags = merge (var.tags, {Name = "${var.env_name}-vpc"})
+}
+
+# Resource - Public Subnet
+resource "aws_subnet" "public_subnet" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = each.value
+availability_zone = each.key
+    map_public_ip_on_launch = true
+    tags = merge (var.tags, {Name = "${var.env_name}-public-subnet"})   
 }
